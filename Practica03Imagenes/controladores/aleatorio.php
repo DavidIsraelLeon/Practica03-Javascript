bdb9bedc6eea8573ff71690c6976a4974efded1f
DavidIsraelLeon <56410422+DavidIsraelLeon@users.noreply.github.com>
2019-11-19 15:04:57 -0500

    Add files via upload

---
+++ Practica03Imagenes/controladores/aleatorio.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <title>Galeria de Imagenes en HTML</title>
+
+    </head>
+
+
+    <body>
+
+    <?php
+        $img = $_GET['img'];
+        echo '<img class="imagenes" src="../images/'. $img . '.jpg" alt="imagen" > '
+    ?>
+        
+        
+
+    </body>
+</html>
